e9fcfa10096ab6e74093dcb220d30219ff46f68a
Angus Lam <angushtlam@gmail.com>
2016-09-21 03:21:05 -0400

    Finished first change

---
+++ js/init.js
@@ -0,0 +1,42 @@
+function startFirefly($) {
+  $.firefly({
+    images: [
+    'assets/firefly/cube_blue.png', 
+    'assets/firefly/cube_green.png'
+    ],
+    minPixel: 5,
+    maxPixel: 30,
+    on: "#hero",
+    total: 20,
+  });
+}
+
+function startParallax($) {
+  $(".parallax").parallax();
+}
+
+// Credits to https://css-tricks.com/snippets/jquery/smooth-scrolling/
+function startSmoothSamePageScrolling($) {
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 600);
+          return false;
+        }
+      }
+    });
+  });
+}
+
+(function ($) {
+  $(document).ready(function () {
+    startFirefly($);
+    startParallax($);
+    startSmoothSamePageScrolling($);
+  });
+})(jQuery);
